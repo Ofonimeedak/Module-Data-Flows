fd42ed0890c29ad8d52c89c88b070b985f73e5f6
ofonimeedak <ofonimeedak@gmail.com>
2026-04-05 20:51:05 +0100

    ex-1-update-argument

--- Sprint-1/destructuring/exercise-1/exercise.js
+++ Sprint-1/destructuring/exercise-1/exercise.js
@@ -6,10 +6,10 @@ const personOne = {
 
 // Update the parameter to this function to make it work.
 // Don't change anything else.
-function introduceYourself(___________________________) {
+function introduceYourself({name,age,favouriteFood}) {
   console.log(
     `Hello, my name is ${name}. I am ${age} years old and my favourite food is ${favouriteFood}.`
   );
 }
 
-introduceYourself(personOne);
+ console.log(introduceYourself(personOne));
