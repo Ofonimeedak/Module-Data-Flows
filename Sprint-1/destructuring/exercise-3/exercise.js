3e0c9681e70c0fcd960de088a0127d1ac15602bb
ofonimeedak <ofonimeedak@gmail.com>
2026-04-05 22:26:48 +0100

    ex-3-print-receipt

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,23 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+
+function orderReceipt(orderItems) {
+  const totalList = [];
+  for (const item of orderItems) {
+    const { itemName, quantity, unitPricePence } = item;
+
+    const total = ((quantity * unitPricePence) / 100).toFixed(2);
+    totalList.push(total);
+    const itemInfo = console.log(`   ${quantity}    ${itemName}     ${total} `);
+  }
+
+  const grandTotal = totalList.reduce((accumulator, current) => {
+    const totalCost = Number(accumulator) + Number(current);
+    return totalCost;
+  }, 0);
+  const finalBill = grandTotal.toFixed(2);
+  console.log(`Total:${finalBill}`);
+}
+
+orderReceipt(order);
